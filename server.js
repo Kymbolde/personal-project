0d86dc326cd7a9601c8c3d4005f42abf51652605
Brayden Kimball <kymboldexb@gmail.com>
2016-12-23 13:54:59 -0700

    api_requests_functional

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var bodyParser = require('body-parser');
 var goodreads = require('goodreads');
 var config = require('./config');
 
-gr = new goodreads.client({ 'key': config.key, 'secret': config.secret });
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -13,8 +11,27 @@ var port = process.env.PORT || 3000;
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+var gr = new goodreads.client({ 
+	'key': config.key,
+	'secret': config.secret 
+})
+
+router.get('/showUser', function(req, res) {
+    gr.showUser('braydenk', function(json) {
+      res.json(json);   
+    })
+});
+
+router.get('/getShelves', function(req, res) {
+	gr.getShelves('62731027', function(json) {
+		res.json(json);
+	})
+});
+
+router.get('/search', function(req, res) {
+	gr.searchBooks('stormlight', function(json) {
+		res.json(json);
+	})
 });
 
 app.use('/api', router);
